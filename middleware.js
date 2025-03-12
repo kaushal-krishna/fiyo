55a07ee6d632696c394197301f7f595a63459fa5
Kaushal Krishna <kaushalkrishna011@gmail.com>
2025-03-12 16:31:41 +0530

    Add Vercel routing configuration for SPA and update middleware fetch behavior

--- middleware.js
+++ middleware.js
@@ -6,9 +6,7 @@ export default async function middleware(req) {
     /(facebookexternalhit|Twitterbot|WhatsApp|Slackbot|LinkedInBot|Googlebot|Pinterestbot|Discordbot|TelegramBot|Bingbot|DuckDuckBot|Yahoo! Slurp|YandexBot|Baiduspider|Applebot|Facebot|ia_archiver|AhrefsBot|SemrushBot|DotBot|MJ12bot|PetalBot|SiteAuditBot|SogouSpider|SeznamBot|Mediapartners-Google|Exabot|GPTBot|ClaudeBot|YouBot|Bytespider|CCBot|MetaInspector|Google-Read-Aloud|Google Favicon|Google Web Preview|AdsBot-Google|Yeti|Twitter AdsBot|Google-Safety|Google-Structured-Data-Testing-Tool|Google-Image|ViberBot|SkypeUriPreview|SkypeBot|SnapchatBot)/i;
 
   if (botRegex.test(userAgent)) {
-    const metadata = await fetch(
-      `${url.origin}/api/handler${url.pathname}${url.search}`
-    )
+    const metadata = await fetch(`${url.origin}/api/handler${url.pathname}${url.search}`)
       .then((res) => res.text())
       .catch(() => null);
 
@@ -19,7 +17,7 @@ export default async function middleware(req) {
     }
   }
 
-  return new Response(null, { status: 204 });
+  return fetch(req);
 }
 
 export const config = { matcher: "/:path*" };

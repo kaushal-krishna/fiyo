7e5e0d5be75a76230ae3b48ed7368bd4c50ba4e2
Kaushal Krishna <kaushalkrishna011@gmail.com>
2025-03-15 09:41:59 +0530

    Update environment configuration: change PRESET_ENV to NODE_ENV in .env file and simplify GraphQL base URI constant in constants.js for consistent production usage.

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,2 @@
-export const FIYOGQL_BASE_URI =
-  import.meta.env.NODE_ENV === "production"
-    ? "https://fiyogql.onrender.com/graphql"
-    : "http://localhost:8000/graphql";
-
+export const FIYOGQL_BASE_URI = "https://fiyogql.onrender.com/graphql"
 export const FIYOSAAVN_BASE_URI = "https://fiyosaavn.vercel.app/api";
